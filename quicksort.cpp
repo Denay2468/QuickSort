ee0b989e7b9410b92dfa8ec0499f1d592ba1f5a4
Denay2468 <denayaung13@gmail.com>
2026-04-10 16:05:52 +0630

    create main function and code

--- quicksort.cpp
+++ quicksort.cpp
@@ -25,3 +25,29 @@ void quickSort(int low, int high) {
         quickSort(pi + 1, high);
     }
 }
+
+int main() {
+    do{
+        cout << "Enter the number of elements (max 20): ";
+        cin >> n;  
+
+        if (n < 1 || n > 20) {
+            cout << "Please enter a valid number of elements (1-20)." << endl;
+        }
+    } while (n < 1 || n > 20);
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    quickSort(0, n - 1);
+
+    cout << "Sorted array:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
